f8b3cccd9327eb39887c43e35a3fef9e61cc0f13
waiferkolar <72coder@gmail.com>
2019-03-29 18:13:16 +0630

    route Modified

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 Route::group(['middleware' => "jwt.auth"], function () {
     Route::get('/user', 'ApiUserController@getUser');
-    Route::get('/product/cat/{id}', 'ApiProductController@getProduct');
+    // Route::get('/product/cat/{id}', 'ApiProductController@getProduct');
     Route::post('/order', 'ApiOrderController@order');
     Route::get('/my_orders', 'ApiOrderController@myorders');
     Route::get('/cats', 'ApiProductController@getAllCats');
@@ -15,3 +15,5 @@ Route::post('/login', 'ApiUserController@login');
 
 Route::post('/imageUpload', 'ApiProductController@imageUpload');
 Route::post('/newProduct', 'ApiProductController@newProduct');
+Route::get('/product/cat/{id}', 'ApiProductController@getProduct');
+
